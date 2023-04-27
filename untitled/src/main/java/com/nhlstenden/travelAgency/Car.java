a232fa0456aa69a1c67278876eb9f018f7a1f746
Chris <chriscrossje2001@gmail.com>
2023-04-27 13:48:12 +0200

    Added a check to see if the license plate is of the right format, added testcases for this method to check if everything works as intended

--- untitled/src/main/java/com/nhlstenden/travelAgency/Car.java
+++ untitled/src/main/java/com/nhlstenden/travelAgency/Car.java
@@ -1,4 +1,8 @@
 package com.nhlstenden.travelAgency;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public abstract class Car {
     private Chauffeur chauffeur;
     private Brand brand;
@@ -30,12 +34,51 @@ public abstract class Car {
         checkLicensePlate(licensePlate);
     }
 
+    /**
+     * Check if a license plate is of the correct format
+     *
+     * @param licensePlate The license plate to check
+     */
     public void checkLicensePlate(String licensePlate) {
-        //TODO check for license plate
+        // Check if the license plate matches the format 999-XX-9
+        Matcher lpMatcher = Pattern.compile("[0-9]{3}-[A-Z]{2}-[0-9]").matcher(licensePlate.toUpperCase());
+        if (lpMatcher.find()) {
+            this.licensePlate = licensePlate.toUpperCase();
+        } else {
+            // Split the string by the - character
+            String[] splitLP = licensePlate.split("-");
+            // Create a new empty String
+            StringBuilder newLP = new StringBuilder();
+            for (int i = 0; i < splitLP.length; i++) {
+                String part = splitLP[i];
+                // Check if the part matches a certain part of the regex
+                if (part.matches("[A-Z]{2}")) {
+                    newLP.append(part);
+                } else if (part.matches("[0-9]{3}")) {
+                    newLP.append(part);
+                } else if (part.matches("[0-9]")) {
+                    newLP.append(part);
+                } else {
+                    // If the first or last (digit) part is incorrect, set the value to -1,
+                    // if the middle (letter) part is incorrect, set the value to null
+                    if (i == 0 || i == 2) {
+                        newLP.append("-1");
+                    } else if (i == 1) {
+                        newLP.append("null");
+                    }
+                }
+                // Add a '-' to the end of each part except for the last part
+                if (i < splitLP.length - 1) {
+                    newLP.append("-");
+                }
+                this.licensePlate = newLP.toString().toUpperCase();
+            }
+        }
     }
 
     /**
      * Calculate the costs of a trip
+     *
      * @param distance The distance of a trip
      * @return The costs of the trip
      */
---
+++ untitled/src/test/java/com/nhlstenden/travelAgency/CarTest.java
@@ -0,0 +1,30 @@
+package com.nhlstenden.travelAgency;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+    private Diesel car;
+    private Chauffeur pieter;
+
+    @BeforeEach
+    void setUp() {
+        pieter = new Chauffeur("Pieter");
+        car = new Diesel(pieter, Brand.VOLVO, "B99-XX-9");
+    }
+
+    @Test
+    void checkLicensePlate() {
+        assertEquals("-1-XX-9", car.getLicensePlate());
+        car.setLicensePlate("999-1X-9");
+        assertEquals("999-NULL-9", car.getLicensePlate());
+        car.setLicensePlate("99A-X9-A");
+        assertEquals("-1-NULL--1", car.getLicensePlate());
+        car.setLicensePlate("AAA-XX-A");
+        assertEquals("-1-XX--1", car.getLicensePlate());
+        car.setLicensePlate("999-X9-A");
+        assertEquals("999-NULL--1", car.getLicensePlate());
+    }
+}
